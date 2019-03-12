9bc9d6223bdb8639e899dc44d3062194ea92a252
ankur1058500 <ankur11.kmr@gmail.com>
2019-03-12 15:43:30 +0530

    Finish layout and routes

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get New" do
-    get signip_path
+    get signup_path
     assert_response :success
   end
 
