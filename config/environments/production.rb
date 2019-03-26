b799584b3c68f029a8bd03900d43b6faed36b9e8
ankur1058500 <ankur11.kmr@gmail.com>
2019-03-26 12:04:25 +0530

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
     :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['GMAIL_USERNAME'],
-    :password       => ENV['GMAIL_PASSWORD'],
+    :user_name      => ENV['GMAIL_SMTP_USER'],
+    :password       => ENV['GMAIL_SMTP_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
